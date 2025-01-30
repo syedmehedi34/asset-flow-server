aba056d7b72ee91738b56becc32a27b0276292bb
syedmehedi34 <syedmeehdi34@gmail.com>
2025-01-31 00:31:46 +0600

    employee register issue solved

--- index.js
+++ index.js
@@ -179,7 +179,7 @@ async function run() {
     });
 
     // post a user data to database
-    app.post("/users", verifyToken, async (req, res) => {
+    app.post("/users", async (req, res) => {
       const user = req.body;
       const query = { email: user.email };
       const existingUser = await userCollection.findOne(query);
